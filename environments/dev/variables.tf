4b1fc7daa5d06129fea57238f9cadd50545d4770
Ashish <ashish.indiabest@gmail.com>
2026-08-09 03:20:00 +0530

    Initial Terraform infrastructure

---
+++ environments/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+
+variable "ssh_public_key" {
+  description = "SSH Public Key"
+  type        = string
+}
